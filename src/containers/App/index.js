2a7701316650046242e653af3800cfc77997d827
Robert <rmolinamir@gmail.com>
2019-05-11 15:53:29 -0400

    Navbar CSS | App.js lazy loading | New loaders during initial app mount | Static files

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,6 @@
 // Libraries
-import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import React, { Suspense } from 'react';
+import styled, { ThemeProvider, css } from 'styled-components';
 
 // Styling
 import 'styles/index.scss';
@@ -8,12 +8,35 @@ import { mainTheme } from 'styles';
 
 // Components
 import { Helmet } from 'react-helmet';
-import Routes from 'containers/Routes';
+import LazyImport from 'components/UI/LazyImport';
+import Loader from 'components/UI/Loader';
+import LogoLoader from 'components/UI/LogoLoader';
 
 const Wrapper = styled.main`
   display: flex;
   flex-flow: column;
   height: 100%;
+`;
+
+const loaderCss = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const Loaders = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledLoader = styled(Loader)`
+  ${loaderCss}
+`;
+
+const StyledLogoLoader = styled(LogoLoader)`
+  ${loaderCss}
 `;
 
 const App = () => (
@@ -25,7 +48,16 @@ const App = () => (
           Denma | Software Development Company
         </title>
       </Helmet>
-      <Routes />
+      <Suspense 
+        fallback={(
+          <Loaders>
+            <StyledLoader size={256} />
+            <StyledLogoLoader />
+          </Loaders>
+        )}
+      >
+        <LazyImport importedComponent={import('containers/Routes')} />
+      </Suspense>
     </Wrapper>
   </ThemeProvider>
 );
